b8ce750ee4a4eb862f868da8a9cd54088a78216f
titusmuthomik <titus.muthomi@tala.co>
2023-06-20 18:59:07 +0300

    Add packages and initialize customer model

---
+++ src/main/java/com/loan/talia/model/Customer.java
@@ -0,0 +1,60 @@
+package com.loan.talia.model;
+
+public class Customer {
+    public  String customerId;
+    public String firstName;
+    public String lastName;
+    public String email;
+    public String phone;
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "customerId='" + customerId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
