9b87964012873ee0fa772d86686eea867d781f7c
titusmuthomik <titus.muthomi@tala.co>
2023-06-20 18:45:19 +0300

    Set up spring app

---
+++ src/main/java/com/loan/talia/TaliaApplication.java
@@ -0,0 +1,13 @@
+package com.loan.talia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaliaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaliaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loan/talia/TaliaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loan.talia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaliaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
